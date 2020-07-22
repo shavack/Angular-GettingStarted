02e464da6fc72708c02b8cf3f2b3b3ec388baa1e
shavack <dkozuch@gmail.com>
2020-07-22 22:23:10 +0200

    Data format with pipes

--- APM-Start/src/app/products/product-list-component.ts
+++ APM-Start/src/app/products/product-list-component.ts
@@ -18,7 +18,7 @@ export class ProductListComponent {
             "productCode": "GDN-0023",
             "releaseDate": "12-02-2005",
             "description": "something",
-            "price": 32.99,
+            "price": 32.99497,
             "starRating": 4.2,
             "imageUrl": "assets/images/garden_cart.png"
         },
@@ -28,7 +28,7 @@ export class ProductListComponent {
             "productCode": "FXA-0023",
             "releaseDate": "12-02-2012",
             "description": "something1",
-            "price": 322.99,
+            "price": 322.9,
             "starRating": 3.2,
             "imageUrl": "assets/images/hammer.png"
         },
@@ -38,7 +38,7 @@ export class ProductListComponent {
             "productCode": "GDN-0023",
             "releaseDate": "12-02-2005",
             "description": "Kill",
-            "price": 55.99,
+            "price": 55,
             "starRating": 2.5,
             "imageUrl": "assets/images/saw.png"
         }
